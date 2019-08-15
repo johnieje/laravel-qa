1e334610b886d6db96532f4135d7123b7c4a5920
John Murungi <johnieje@yahoo.com>
2019-08-15 13:46:02 +0300

    Added author information and question create date

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -12,14 +12,18 @@
                         <div class="media">
                             <div class="media-body">
                                 <h3 class="mt-0">
-                                    {{ $question->title }}
+                                    <a href="{{ $question->url }}">{{ $question->title }}</a>
                                 </h3>
+                                <p class="lead">
+                                    Asked by <a href="{{ $question->user->url}}">{{ $question->user->name }}</a>
+                                    <small class="text-muted">{{ $question->created_date }}</small>
+                                </p>
                                 {{ str_limit($question->body, 250) }}
                             </div>
                         </div>
                         <hr>
                     @endforeach
-                    <div class="text-center">
+                    <div>
                         {{ $questions->links() }}
                     </div>
                 </div>
